5b4209c635aa0d199b259d05a312512f89378053
WarrenDavies <warren_davies81@hotmail.com>
2018-09-16 16:34:25 +0100

    set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+gulp.task('sass', function(){
+    return gulp.src('style.scss')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('./public'))
+  });
+
+gulp.task('watch', function(){
+    gulp.watch('style.scss', ['sass']); 
+    // Other watchers
+})
